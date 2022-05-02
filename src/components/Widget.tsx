12a3ba4f09b22afe14014e29cf0a76913a689680
Cleisson <cleissonconstc@gmail.com>
2022-05-02 17:25:36 -0300

    Creating the state to open the widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
+   const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+   function toggleWidgetVisibility() {
+      setIsWidgetOpen(!isWidgetOpen)
+   }
+
    return (
       <div className='absolute bottom-5 right-5'>
-         <button className='bg-brand rounded-full p-3 h-12 flex items-center justify-center text-white group'>
+         {isWidgetOpen && <p>Hello World!</p>}
+
+         <button onClick={toggleWidgetVisibility} className='bg-brand rounded-full p-3 h-12 flex items-center justify-center text-white group'>
             <ChatTeardropDots className='w-6 h-6' weight="regular" color='white' />
             <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-in-out'>
                <span className='pl-1'>
